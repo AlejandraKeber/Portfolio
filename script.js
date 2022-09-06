be9b693c2231b0bd19421ebb8b127230abe4dd18
AlejandraKeber <alejandrakeber@gmail.com>
2022-09-06 13:23:53 -0500

    Fix Stylelint errors

--- script.js
+++ script.js
@@ -2,21 +2,22 @@
   const hamburger = document.querySelector('.hamburger');
   const modalMenu = document.querySelector('.nav-items');
   hamburger.addEventListener('click', () => {
-   // console.log('Hello world')
     modalMenu.style.left = 0;
   });
 
 const closeMenu = document.querySelector('.close');
 closeMenu.addEventListener('click', () => {
- // console.log('Hello world')
   modalMenu.style.left = '-100%';
 });
 
- modalMenu.forEach((n) =>
+/* const closePortfolio = document.querySelector('#Portfolio');
+closePortfolio.addEventListener('click', () => {
+  modalMenu.style.left = '-100%';
+}); */
+
+  modalMenu.forEach((n) =>
    n.addEventListener('click', () => {
-     humburger.classList.remove('active');
+     hamburger.classList.remove('active');
      nav.classList.remove('active');
-   })
- );
-}
+   }));
 

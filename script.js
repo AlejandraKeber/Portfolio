3eff635db3265d735a81d9030643c9a60aa2898b
AlejandraKeber <alejandrakeber@gmail.com>
2022-09-06 09:52:19 -0500

    Add js to close menu

--- script.js
+++ script.js
@@ -2,25 +2,13 @@
   const hamburger = document.querySelector('.hamburger');
   const modalMenu = document.querySelector('.nav-items');
   hamburger.addEventListener('click', () => {
-    console.log('Hello world')
+   // console.log('Hello world')
     modalMenu.style.left = 0;
   });
 
-
 const closeMenu = document.querySelector('.close');
 closeMenu.addEventListener('click', () => {
-  console.log('Hello world')
-  modalMenu.style.display = 'block';
+ // console.log('Hello world')
+  modalMenu.style.left = '-100%';
 });
 
-
-
-/* function myFunction() {
-  var x = document.getElementById('.nav-items');
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
-*/
